56822ebb58df5bc407db304e4926388248a96037
Manav Agarwal <manavagarwal193@gmail.com>
2026-09-19 17:44:07 +0530

    feat: integrate consent-aware memory and NIMO knowledge

---
+++ learning/consent_gate.py
@@ -0,0 +1,6 @@
+"""Hard consent gate for reusable learning data."""
+from security.consent import Purpose
+class LearningConsentGate:
+    def __init__(self,consent_manager): self.consents=consent_manager
+    def require_learning(self): self.consents.require(Purpose.LEARNING_CONTRIBUTION)
+    def require_model_improvement(self): self.consents.require(Purpose.MODEL_IMPROVEMENT)
